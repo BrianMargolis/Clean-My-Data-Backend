6b6ac6092ee2f73d0347120b89732fee9daea633
Brian Margolis <brian.m.margolis@gmail.com>
2017-10-31 23:29:20 -0500

    Basic implementation of receiving a file

--- server.py
+++ server.py
@@ -1,8 +1,17 @@
-from flask import Flask
+from flask import Flask, request
 from flask_cors import CORS
+
 app = Flask(__name__)
-CORS(app)
+
 
 @app.route('/', methods=['GET', 'POST'])
 def hello_world():
-    return 'Hello, World!'
+    if request.method == 'POST':
+        file = next(iter(request.files.values()))
+        file.save()
+    return
+
+
+if __name__ == '__main__':
+    CORS(app)
+    app.run(debug=True)
