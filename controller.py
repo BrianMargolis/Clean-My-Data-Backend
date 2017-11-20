75b4a066ff26a0c57ae54613165b2cb9700a5197
Brian Margolis <brian.m.margolis@gmail.com>
2017-11-20 13:46:56 -0600

    Save file more sanely, return modified file

--- controller.py
+++ controller.py
@@ -1,9 +1,12 @@
 import random
 import string
 
-from flask import Flask, request, jsonify
+import os
+from flask import Flask, request, jsonify, send_from_directory
 from flask_cors import CORS
 from flask_login import LoginManager
+
+from Cleaner import Cleaner
 
 app = Flask(__name__)
 login_manager = LoginManager()
@@ -19,23 +22,29 @@ def upload_file():
             return {"message": "{0} files is too many. Send 1.".format(len(request.files))}, 400
 
         file = next(iter(request.files.values()))  # get the single file
-        file_id = get_file_id()
-        file.save(file_id)
-        return jsonify({"file_id": file_id})
+        file_name = get_file_name()
+        file.save(os.path.join(app.config['UPLOAD_FOLDER'], file_name))
+        return jsonify({"file_name": file_name})
 
-@app.route("/outlier_detection", methods=['POST'])
-def outlier_detection():
+
+@app.route("/clean", methods=['POST'])
+def clean():
     if request.method == "POST":
-        a = request.query_string
-        print(a)
-        return "asdf"
+        json = request.json
+        file_name = json["file_name"]
+        options = json["options"]
+        cleaner = Cleaner(file_name, options)
+        return send_from_directory(app.config['UPLOAD_FOLDER'], file_name)
 
-def get_file_id():
+
+def get_file_name():
     N = 15
     id = ''.join(random.choice(string.ascii_uppercase + string.digits) for _ in range(N))
     return id + ".csv"
 
+
 if __name__ == '__main__':
     login_manager.init_app(app)
     CORS(app)
+    app.config['UPLOAD_FOLDER'] = app.root_path + '/uploads'
     app.run(debug=True)
