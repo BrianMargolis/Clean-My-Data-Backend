bd9c10801708e915439c6433fa0416976abc825b
Brian Margolis <brian.m.margolis@gmail.com>
2017-12-04 12:51:19 -0600

    Wire summary stats up to error_detection lib

--- error_detection/error_detection.py
+++ error_detection/error_detection.py
@@ -1,5 +1,6 @@
 import numpy as np
 import error_detection.vector_operations as vector_ops
+import error_detection.summary as summary
 
 
 def duplicates(matrix, fuzzy=False, fuzzy_ratio_threshold=70):
@@ -14,13 +15,13 @@ def outliers(matrix):
     return extend_vector_op_to_matrix(matrix, vector_ops.outliers)
 
 
-def extend_vector_op_to_matrix(input_matrix, vector_op, vector_op_args=()):
-    output_matrix = []
+def extend_vector_op_to_matrix(matrix, vector_op, vector_op_args=()):
+    return np.array([vector_op(column, *vector_op_args) for column in matrix.T]).T
 
-    # Is transposing a huge matrix expensive?
-    # https://stackoverflow.com/questions/19479384/is-numpy-transpose-reordering-data-in-memory/19479436#19479436
-    # TLDR: no, it just changes the stride of the array.
-    for columns in input_matrix.T:
-        output_matrix.append(vector_op(columns, *vector_op_args))
 
-    return np.array(output_matrix).T
+def get_column_error_rates(error_matrix):
+    return summary.get_column_error_rates(error_matrix)
+
+
+def get_column_statistics(data_matrix, alpha=.95):
+    return summary.get_column_statistics(data_matrix, alpha)
