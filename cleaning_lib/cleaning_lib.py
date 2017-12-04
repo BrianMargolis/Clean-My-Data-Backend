38ad0524a1f83ffe3e340ffb1b1b7e060200632e
Brian Margolis <brian.m.margolis@gmail.com>
2017-12-04 11:51:40 -0600

    Clean up cleaning library

--- cleaning_lib/cleaning_lib.py
+++ cleaning_lib/cleaning_lib.py
@@ -5,82 +5,52 @@ from fuzzywuzzy import fuzz
 from sklearn.ensemble import IsolationForest as isolate
 
 
+def extend_vector_op_to_matrix(input_matrix, vector_op, vector_op_args=[]):
+    output_matrix = []
+
+    # Is transposing a huge matrix expensive?
+    # https://stackoverflow.com/questions/19479384/is-numpy-transpose-reordering-data-in-memory/19479436#19479436
+    # TLDR: no, it just changes the stride of the array.
+    for columns in input_matrix.T:
+        output_matrix.append(vector_op(columns, *vector_op_args))
+
+    return np.array(output_matrix).T
+
+
 # Per-column cleaning operations
 
 
-def fuzzy_match_column(column):
-    matched = []
-    error_col = column
-    for x in range(len(column)):
-        for y in range(x + 1, len(column)):
-            fuzzyRatio = fuzz.ratio(column[x], column[y])
-            if fuzzyRatio > 70:
-                error_col[x] = 1
-            else:
-                error_col[x] = 0
-    return error_col
+def duplicates(vector, fuzzy=False, fuzz_ratio_threshold=70):
+    error = np.zeros_like(vector)
+
+    for x in range(len(vector)):
+        for y in range(x + 1, len(vector)):
+            error[x] = fuzz.ratio(x, y) > fuzz_ratio_threshold if fuzzy else x == y
+
+    return error
 
 
-def check_type_column(column):
-    # we need to check to see if the column types match up, but everything comes in as unicode
-    # we try to convert each element in the column to a float. strings cannot be converted to floats, so we will convert it to a string instead.
-    numberCount = 0
-    letterCount = 0
-    numberRows = []
-    letterRows = []
-    col_type = "number"
-    error_col = column
+def wrong_types(vector):
+    numbers = np.zeros_like(vector)
+    strings = np.zeros_like(vector)
 
-    for x in range(len(column)):
+    for i, item in enumerate(vector):
         try:
-            column[x] = float(column[x])
-            numberRows.append(x)
-            numberCount += 1
+            float(item)
+            numbers[i] = True
         except ValueError:
-            column[x] = str(column[x])
-            letterRows.append(x)
-            letterCount += 1
+            strings[i] = True
 
-    if letterCount > numberCount:
-        col_type = "letter"
-    for x in range(len(column)):
-        if col_type == "letter":
-            if x in numberRows:
-                error_col[x] = 1
-            else: error_col[x] = 0
-        else:
-            if x in letterRows:
-                error_col[x] = 1
-            else: error_col[x] = 0
-    return error_col
+    is_numbers = numbers.sum() > strings.sum()
+    return strings if is_numbers else numbers
 
-def outlier_detection_column(col):
-    # isolation forest to detect outliers, options to be added
-    arr = np.asarray(col)
-    arr = arr.reshape(-1, 1)
+
+def outliers(vector):
+    vector = vector.reshape(-1, 1)
+
     forest = isolate()
-    forest.fit(arr)
-    forest_outliers = forest.predict(arr)
-    outlier_index = []
-    for i in range(len(forest_outliers)):
-        if forest_outliers[i] == -1:
-            outlier_index.append(i)
-    error_col = col
-    for i in range(len(error_col)):
-        if i in outlier_index:
-            error_col[i] = 1
-        else: error_col[i] = 0
-    return error_col
+    forest.fit(vector)
+    forest_outliers = forest.predict(vector)
+    error = [forest_outliers == 1]
 
-
-
-# Utilities
-
-def lower_all(col):
-    # standardize cases for further up detection/ row combine
-    return [item.lower for item in col]
-
-
-def strip_clean(col):
-    # strip clean, for example ice_cream and ice cream is the same thing
-    return [" ".join(re.split('\s|; |, |\*|\n|_|-', item)) for item in col]
+    return error
